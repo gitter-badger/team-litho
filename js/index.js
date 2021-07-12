2a1eb43e5df01bb1b5c78cc0fd4cf95a14b7d032
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-12 19:05:08 -0400

    inroyable

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/*
+ * @fileoverview Utilities for manipulating JavaScript code.
+ * @author joey@tech-center.com (Joey Cadieux)
+ */
+
+const ourtime = new Date();
+document.getElementById("ourtime").innerHTML = ourtime;
