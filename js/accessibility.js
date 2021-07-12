3d46ec6708c6bb4062e0e2d92cb13088daf3cdac
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-12 18:18:00 -0400

    adding js code

--- js/accessibility.js
+++ js/accessibility.js
@@ -1,46 +1,10 @@
-const virusData = [
-  {
-    id: 42001,
-    Country: "Canada",
-    cases: 0,
-    status: 0,
-    dateOfFirstCase: "2019-12-31",
-    dateOfDisasterDeclaration: null,
-    dailyOverview: [
-      {
-        date: "20-03-18",
-        cases: 0
-      },
-      {
-        date: "2020-03-19",
-        cases: 1
-      },
-      {
-        date: "2020-03-20",
-        cases: 4
-      },
-      {
-        date: "2020-03-21",
-        cases: 4
-      },
-      {
-        date: "2020-03-22",
-        cases: 5
-      },
-      {
-        date: "2020-03-23",
-        cases: 6
-      },
-      {
-        date: "2020-03-24",
-        cases: 6
-      },
-      {
-        date: "2020-03-25",
-        cases: 6
-      }
-    ]
-  }
-];
+/*
+ * @fileoverview Utilities for manipulating JavaScript code.
+ * @author joey@tech-center.com (Joey Cadieux)
+ */
 
-console.log(virusData);
+const timezone = new Date();
+document.getElementById("timezone").innerHTML = timezone;
+
+const demo = ["Web Developer", "Front End Developer", "Back End Developer"];
+document.getElementById("demo").innerHTML = demo;
