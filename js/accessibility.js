6c0431cc08d6aad344d82611a87d042d10ca8249
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-12 18:57:21 -0400

    INC......

--- js/accessibility.js
+++ js/accessibility.js
@@ -8,3 +8,14 @@ document.getElementById("timezone").innerHTML = timezone;
 
 const demo = ["Web Developer", "Front End Developer", "Back End Developer"];
 document.getElementById("demo").innerHTML = demo;
+
+// console.log(timezone);
+// console.log(demo);
+
+const posted = ["Hello World", "DevOps"];
+document.getElementById("posted").innerHTML = posted;
+
+// Background Color /* accessbility.html -> .js */
+
+/* const ChangeColor = document.getElementById("ChangeColor");
+ChangeColor.body.backgroundColor = "Blue"; */
