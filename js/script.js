e08e22aec867a7b20b6da3bf9ebb80996887fb8b
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-13 11:05:07 -0400

    change message in the console.

--- js/script.js
+++ js/script.js
@@ -14,6 +14,6 @@ var cssRule =
   "text-shadow: none;" +
   "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
 console.log(
-  "%c teamlitho.com/teamlitho/self-xss/info.html Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.Do not enter or paste code that you do not understand.",
+  "%c Attention! Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.Do not enter or paste code that you do not understand.",
   cssRule
 );
