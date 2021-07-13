cf7e907c83fa2e133873fd0dcd6749651bd1db75
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-13 10:55:13 -0400

    index.html debugging mode

--- js/accessibility.js
+++ js/accessibility.js
@@ -8,8 +8,8 @@ document.getElementById("timezone").innerHTML = timezone;
 const demo = ["Web Developer", "Front End Developer", "Back End Developer"];
 document.getElementById("demo").innerHTML = demo;
 
-// console.log(timezone);
-// console.log(demo);
+console.log(timezone);
+console.log(demo);
 
 // const timezone1 = new Date();
 // document.getElementById("timezone1").innerHTML = timezone1;
