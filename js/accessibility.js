586dd70c444cf6b5dd2f76c7a3491f34353cc538
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-13 14:33:30 -0400

    inc

--- js/accessibility.js
+++ js/accessibility.js
@@ -8,14 +8,17 @@ document.getElementById("timezone").innerHTML = timezone;
 const demo = ["Web Developer", "Front End Developer", "Back End Developer"];
 document.getElementById("demo").innerHTML = demo;
 
-console.log(timezone);
-console.log(demo);
+// console.log(timezone);
+// console.log(demo);
 
 // const timezone1 = new Date();
 // document.getElementById("timezone1").innerHTML = timezone1;
 
 const posted = ["Hello World", "DevOps"];
 document.getElementById("posted").innerHTML = posted;
+
+const footerp = new Date();
+document.getElementById("footerp").innerHTML = footerp;
 
 // console.log(posted);
 
