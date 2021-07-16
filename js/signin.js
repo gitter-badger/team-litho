10ba1b333984d8900c2726316d1cdaa061174489
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-16 09:58:21 -0400

    secure pages that need auth

--- js/signin.js
+++ js/signin.js
@@ -1,3 +1,13 @@
 function Login() {
   window.alert("It is working");
 }
+
+// var custom = [
+//   "/pages",
+//   "/index.html",
+//   "/ifttt.html",
+//   "/forums.html",
+//   "/alexa.html",
+//   "google-home.html",
+//   "/map.html"
+// ];
