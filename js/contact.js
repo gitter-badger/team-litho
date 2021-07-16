7f1bafb9fa7436e491a85def29ef9e4c4e556069
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-16 13:54:47 -0400

    removing some unsed file

--- js/contact.js
+++
@@ -1,8 +0,0 @@
-/*
- * @fileoverview Utilities for manipulating JavaScript code.
- * @author joey@tech-center.com (Joey Cadieux)
- */
-
-var FullName = document.getElementById("fName").value;
-var Email = document.getElementById("email").value;
-var ContactForm = document.getElementById("msg").value;
--- js/message.console.js
+++
@@ -1,6 +0,0 @@
-var consoleCss =
-  "color: green;" +
-  "font-size: 20px;" +
-  "font-weight: bold;" +
-  "text-shadow: 1px;" +
-  console.log("%cWarning!", consoleCss);
