5757d817d6b0c8066e335f77f11bc7fd8488e187
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-18 12:50:11 -0400

    inc.

---
+++ js/map.js
@@ -0,0 +1,15 @@
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tyOWY0MmNqNDh1YTJ2dDljbmJsNmZmMyJ9.5nGWLKOnVMVjeSy1kFHdNg}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/dark-v10",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tyOWY0MmNqNDh1YTJ2dDljbmJsNmZmMyJ9.5nGWLKOnVMVjeSy1kFHdNg"
+  }
+).addTo(mymap);
+
+var mymap = L.map("mapid").setView([51.505, -0.09], 13);
