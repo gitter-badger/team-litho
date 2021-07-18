a84fba11cf501fbdfe5d6ce5623db5a90b309800
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-18 17:11:33 -0400

    testing javascript function.

---
+++ js/content.js
@@ -0,0 +1,9 @@
+/*
+ * @fileoverview Utilities for manipulating JavaScript code.
+ * @author joey@tech-center.com (Joey Cadieux)
+ */
+
+function ourContent() {
+  window.prompt("What is your Github Username!");
+}
+ourContent();
