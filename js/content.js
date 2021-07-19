7334c85273c47ed37259a969b5dc182a9c056d9a
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-18 20:41:23 -0400

    in.cc

--- js/content.js
+++ js/content.js
@@ -2,8 +2,3 @@
  * @fileoverview Utilities for manipulating JavaScript code.
  * @author joey@tech-center.com (Joey Cadieux)
  */
-
-function ourContent() {
-  window.prompt("What is your Github Username!");
-}
-ourContent();
