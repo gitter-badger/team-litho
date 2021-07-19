cea9693279c5eb2e3bc989a758fc2b5674069eec
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-19 19:21:02 -0400

    inc.

--- js/content.js
+++ js/content.js
@@ -2,3 +2,10 @@
  * @fileoverview Utilities for manipulating JavaScript code.
  * @author joey@tech-center.com (Joey Cadieux)
  */
+
+var offcanvasElementList = [].slice.call(
+  document.querySelectorAll(".offcanvas")
+);
+var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+  return new bootstrap.Offcanvas(offcanvasEl);
+});
