e677772e7a5c92fc94361d2b4d1e54ed80cd8ddb
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-07-24 11:32:08 -0400

    set hours

--- js/index.js
+++ js/index.js
@@ -3,10 +3,6 @@
  * @author joey@tech-center.com (Joey Cadieux)
  */
 
-var moment = require("moment-timezone");
-moment().tz("America/Los_Angeles").format();
-
+const moment = new Date();
+moment.setHours();
 document.getElementById("moment").innerHTML = moment;
-
-// const ourtime = new Date();
-// document.getElementById("ourtime").innerHTML = ourtime;
