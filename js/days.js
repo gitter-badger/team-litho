c75522fe72426ea7696431777fa6b907bb00e159
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-10-07 21:32:46 +0000

    change license statement

--- js/days.js
+++ js/days.js
@@ -1,2 +1,7 @@
+/*
+ * @fileoverview Utilities for manipulating JavaScript code.
+ * @author joey@tech-center.com (Joey Cadieux)
+ */
+
 const days = moment().add(5, 'days').calendar(); 
 document.getElementById("days").innerHTML = days;
