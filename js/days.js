8f00a6204adef5be5401274affcd30acc1b8913d
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-10-15 15:25:51 +0000

    changing H1 for javascript classes.

--- js/days.js
+++ js/days.js
@@ -5,3 +5,14 @@
 
 const days = moment().add(5, 'days').calendar(); 
 document.getElementById("days").innerHTML = days;
+
+
+class Websitetitle {
+	constructor(text, title) {
+		this.text = text;
+		this.title = title;
+	} 
+}
+
+const Websitetitle = new Websitetitle(­­'TeamLitho', 'Developer');
+document.getElementById('demo').innerHTML = myWebsitetitle.text + " " + myWebsitetitle.title;
