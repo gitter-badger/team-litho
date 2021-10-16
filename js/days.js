37c3771ce0ea548b4a7cfa84db52365a07fdbcbb
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-10-16 22:48:29 +0000

    adding title

--- js/days.js
+++ js/days.js
@@ -3,16 +3,5 @@
  * @author joey@tech-center.com (Joey Cadieux)
  */
 
-const days = moment().add(5, 'days').calendar(); 
+const days = moment().add(5, 'days').calendar();
 document.getElementById("days").innerHTML = days;
-
-
-class Websitetitle {
-	constructor(text, title) {
-		this.text = text;
-		this.title = title;
-	} 
-}
-
-const Websitetitle = new Websitetitle(­­'TeamLitho', 'Developer');
-document.getElementById('demo').innerHTML = myWebsitetitle.text + " " + myWebsitetitle.title;
