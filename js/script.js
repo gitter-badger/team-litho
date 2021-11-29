ddb32cbb69d55bf2380a4f6ff9b45478335ad1d2
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-11-29 19:23:40 +0000

    inu.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var cssRule =
   "color: red;" +
   "font-size: 50px;" +
   "font-weight: bold;" +
-  "text-shadow: 2px 2px red;" +
+  "text-shadow: 2px 2px black;" +
   "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
 console.log("%cStop!", cssRule);
 
