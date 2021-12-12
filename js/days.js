64071b12d1e7c16eecd4f5435816f115020e6c33
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2021-12-12 22:53:34 +0000

    inv.

--- js/days.js
+++
@@ -1,7 +0,0 @@
-/*
- * @fileoverview Utilities for manipulating JavaScript code.
- * @author joey@tech-center.com (Joey Cadieux)
- */
-
-const days = moment().format('LTS');
-document.getElementById("days").innerHTML = days;
