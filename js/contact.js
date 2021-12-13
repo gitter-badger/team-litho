f4f91df929ce407fe0a83355b6e16eb857d59bfc
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2021-12-13 22:04:47 +0000

    inv.

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+/*
+ * @fileoverview Utilities for manipulating JavaScript code.
+ * @author joey@tech-center.com (Joey Cadieux)
+ */
+
+const contactpage = moment().format('LTS');
+document.getElementById("contactpage").innerHTML = contactpage; 
