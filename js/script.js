ba5872eff399e75c80bfbfb5cc36b5894a191b67
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2021-12-20 19:56:07 +0000

    ino.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 var cssRule =
-  "color: red;" +
-  "font-size: 70px;" +
-  "font-weight: bold;" +
-  "text-shadow: 1px 1px 10px rgb(0, 0, 0);" +
-  "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
-console.log("%cStop! TeamLitho | Developer", cssRule);
+	"color: red;" +
+	"font-size: 70px;" +
+	"font-weight: bold;" +
+	"text-shadow: 1px 1px 10px rgb(0, 0, 0);" +
+	"filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+console.log("%cStop!", cssRule);
 
 var cssRule =
-  "color: black;" +
-  "font-size: 20px;" +
-  "font-weight: arial;" +
-  "text-shadow: none;" +
-  "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+	"color: red;" +
+	"font-size: 70px;" +
+	"font-weight: bold;" +
+	"text-shadow: 1px 1px 10px rgb(20, 0, 208);" +
+	"filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
+console.log("%cTeamLitho | Developer", cssRule);
+
+var cssRule =
+	"color: black;" +
+	"font-size: 20px;" +
+	"font-weight: arial;" +
+	"text-shadow: none;" +
+	"filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
 console.log(
-  "%c Attention! Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.Do not enter or paste code that you do not understand.",
-  cssRule
+	"%c Attention! Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.Do not enter or paste code that you do not understand.",
+	cssRule
 );
