3f40bf51b358751840cf4b8407e8583c26eb4dfd
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2021-12-22 00:09:52 +0000

    inc.

--- js/contact.js
+++ js/contact.js
@@ -5,3 +5,26 @@
 
 const contactpage = moment().format('LTS');
 document.getElementById("contactpage").innerHTML = contactpage;
+
+
+// Contact Form
+
+const __email = __email;
+document.getElementById("__email").innerHTML = __email;
+
+
+const __msg = __msg;
+document.getElementById("__msg").innerHTML = __msg;
+
+
+function validateMsg(msg) {
+	if (typeof msg !== 'string' || msg.lenth < 10) {
+		return 'Message: Whoops!'
+	}
+}
+
+function validateEmail(email) {
+	if (typeof email !== 'string' || email.lenth < 3) {
+		return 'Email: Whoops!'
+	}
+}
