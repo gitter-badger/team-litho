d22e306f52354fc8fbb38e757be449d3f8f06efe
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-01-02 18:06:46 +0000

    inc.

--- config.js
+++ config.js
@@ -1,31 +1,11 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2t4ZHI0YThiMTdzMjJva3kybnZxZzhkNyJ9.2wZYl3GR7if3ucmVBMoAFA';
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2t4eGp2cTVpMmFsbzJvbnliaTZheHRtdCJ9.Dhq-8dqBOQ9rzbNgdymJ3A';
 const map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/dark-v10',
 	center: [-96.052335, 39.159882],
-	zoom: 3.5
+	zoom: 4.3
 });
 
 
-disable map zoom when using scroll
+// disable map zoom when using scroll
 map.scrollZoom.disable();
-
-
-
-map.on('load', () => {
-	map.addSource('spcday1', {
-		type: 'geojson',
-		data: 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
-	});
-
-	map.addLayer({
-		'id': 'spcday1-layer',
-		'type': 'fill',
-		'source': 'spcday1',
-		'paint': {
-			'fill-color': ['get', 'fill'],
-			'fill-outline-color': ['get', 'stroke'],
-			'fill-opacity': 0.5
-		}
-	});
-});
