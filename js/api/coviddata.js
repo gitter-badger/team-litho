84669dbc2b31b5aed40ffaef910da5938e04a1d7
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-01-05 19:11:47 +0000

    inm.

---
+++ js/api/coviddata.js
@@ -0,0 +1,10 @@
+/*
+ * @fileoverview Utilities for manipulating JavaScript code.
+ * @author joey@tech-center.com (Joey Cadieux)
+ */
+
+fetch('https://api.covid19api.com/countries')
+  .then(response => response.json())
+  .then(data => {
+    const { places, reports } = data;
+  })
