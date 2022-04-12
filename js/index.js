89b80fedbc66fffdfa19a23f250f3eec3d10be06
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2022-04-12 15:48:51 +0000

    inm.

--- js/index.js
+++
@@ -1,7 +0,0 @@
-/*
- * @fileoverview Utilities for manipulating JavaScript code.
- * @author joey@tech-center.com (Joey Cadieux)
- */
-
-const timeupdater = moment().format('MMMM Do YYYY, h:mm:ss a');
-document.getElementById("timeupdater").innerHTML = timeupdater;
